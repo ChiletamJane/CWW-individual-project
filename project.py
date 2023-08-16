5f3f3d4e30cce0ab96ba10fd22e462f5c518342b
Iwuagwu Jane <nathanchiletam@gmail.com>
2023-08-16 14:36:53 +0100

    first commit
    Signed-off-by: Iwuagwu Jane <nathanchiletam@gmail.com>

---
+++ project.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.read_csv(r'C:/Users/HP HP/Desktop/CWW/netflix_data.csv')
+
+print(df)
+import pandas as pd
+df=pd.read_csv(r'C:/Users/HP HP/Desktop/CWW/color_data.csv')
+print(df)
